df697f7da45b8b71581675f615dfebe0ab7f7958
EduardoDosSantosFerreira <eduardosferreira69@gmail.com>
2024-03-31 23:47:17 -0300

    update_v2

--- js/index.js
+++ js/index.js
@@ -2,6 +2,97 @@
 function adicionarLoginAoDOM() {
     var div = document.createElement("div");
     div.innerHTML = `
+    <main>
+    <section class="home" id="home">
+        <div class="home-text">
+            <h1>Food Tasty</h1>
+            <h2>The tasty food of <br> your choice</h2>
+            <a href="#menu" class="btn">View Menu</a>
+        </div>
+        <div class="home-img">
+            <img src="img/home.png" alt="">
+        </div>
+    </section>
+    <section class="about" id="about">
+        <div class="about-img">
+            <img src="img/about.jpg" alt="">
+        </div>
+        <div class="about-text">
+            <span>About Us</span>
+            <h2>We make good and <br> tasty food</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut quo iure rem ducimus tenetur deserunt commodi doloribus, quibusdam vel fuga?</p>
+            <a href="#" class="btn">Lear More</a>
+        </div>
+    </section>
+    <section class="menu" id="menu">
+        <div class="heading">
+            <span>Menu</span>
+            <h2>Tasty menu of the week</h2>
+        </div>
+        <div class="menu-container">
+            <div class="box">
+                <div class="box-img">
+                    <img src="img/menu1.png" alt="">
+                </div>
+                <h2>Cheese Burger</h2>
+                <h3>Tasty Food</h3>
+                <span>$12.05</span>
+                <i class='bx bx-cart-alt'></i>
+            </div>
+            <div class="box">
+                <div class="box-img">
+                    <img src="img/menu2.png" alt="">
+                </div>
+                <h2>Cheese Burger</h2>
+                <h3>Tasty Food</h3>
+                <span>$12.05</span>
+                <i class='bx bx-cart-alt'></i>
+            </div>
+            <div class="box">
+                <div class="box-img">
+                    <img src="img/menu3.png" alt="">
+                </div>
+                <h2>Cheese Burger</h2>
+                <h3>Tasty Food</h3>
+                <span>$12.05</span>
+                <i class='bx bx-cart-alt'></i>
+            </div>
+        </div>
+    </section>
+    <section class="services" id="services">
+        <div class="heading">
+            <span>Services</span>
+            <h2>We provide best food services</h2>
+        </div>
+        <div class="servives-container">
+            <div class="s-box">
+                <img src="img/order.png" alt="">
+                <h3>You Order</h3>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sit ea fugiat esse tempore ipsum temporibus.</p>
+            </div>
+            <div class="s-box">
+                <img src="img/delivery-truck.png" alt="">
+                <h3>Shipping</h3>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sit ea fugiat esse tempore ipsum temporibus.</p>
+            </div>
+            <div class="s-box">
+                <img src="img/shipping.png" alt="">
+                <h3>Delivered</h3>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sit ea fugiat esse tempore ipsum temporibus.</p>
+            </div>
+        </div>
+    </section>
+    <section class="connect">
+        <div class="connect-text">
+            <span>Let's Talk</span>
+            <h2>Connect now</h2>
+        </div>
+        <a href="#" class="btn">Contact Us</a>
+    </section>
+    <div class="copyright">
+        <p>&#169; Eduardo S Ferreira All Right Reserved.</p>
+    </div>
+    </main>
     <!-- Contact -->
     <section class="contact" id="contact">
         <div class="contact-box">
--- js/main.js
+++ js/main.js
@@ -1,18 +1,13 @@
-// Navbar
 let menu = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.navbar');
-
 menu.onclick = () => {
     menu.classList.toggle('bx-x');
     navbar.classList.toggle('active');
 }
-
 window.onscroll = () => {
     navbar.classList.remove('active');
 }
-// Dark Mode
 let darkmode = document.querySelector('#darkmode');
-
 darkmode.onclick = () => {
     if(darkmode.classList.contains('bx-moon')){
         darkmode.classList.replace('bx-moon','bx-sun');
@@ -22,16 +17,12 @@ darkmode.onclick = () => {
         document.body.classList.remove('active');
     }
 }
-
-// Scroll Reveal
 const sr = ScrollReveal ({
     origin: 'top',
     distance: '40px',
     duration: 2000,
     reset: true
 });
-
-
 sr.reveal(`.home-text, .home-img,
             .about-img, .about-text,
             .box, .s-box,
