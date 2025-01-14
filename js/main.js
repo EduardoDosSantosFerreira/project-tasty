595c28453d403bea2339c67b9c31af3e306b9470
EduardoDosSantosFerreira <eduardosferreira69@gmail.com>
2025-01-14 15:12:15 -0300

    update_v3

--- js/main.js
+++ js/main.js
@@ -1,32 +1,38 @@
 let menu = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.navbar');
+
 menu.onclick = () => {
     menu.classList.toggle('bx-x');
     navbar.classList.toggle('active');
 }
+
 window.onscroll = () => {
     navbar.classList.remove('active');
 }
+
 let darkmode = document.querySelector('#darkmode');
+
 darkmode.onclick = () => {
-    if(darkmode.classList.contains('bx-moon')){
-        darkmode.classList.replace('bx-moon','bx-sun');
+    if (darkmode.classList.contains('bx-moon')) {
+        darkmode.classList.replace('bx-moon', 'bx-sun');
         document.body.classList.add('active');
-    }else{
-        darkmode.classList.replace('bx-sun','bx-moon');
+    } else {
+        darkmode.classList.replace('bx-sun', 'bx-moon');
         document.body.classList.remove('active');
     }
 }
-const sr = ScrollReveal ({
+
+const sr = ScrollReveal({
     origin: 'top',
     distance: '40px',
     duration: 2000,
     reset: true
 });
+
 sr.reveal(`.home-text, .home-img,
             .about-img, .about-text,
             .box, .s-box,
             .btn, .connect-text,
             .contact-box`, {
     interval: 200
-})
+});
